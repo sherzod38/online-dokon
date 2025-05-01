1295275a3faa89c8e436d52fd2af831469e24849
sherzod38 <nadirovsherzod82@gmil.com>
2025-05-01 12:57:57 +0500

    f8

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,7 @@ import { ContactForm } from "@/components/contact-form";
 export default function ContactPage() {
   return (
     <main className="container mx-auto py-8">
-      <h1 className="text-3xl font-bold mb-8 text-center">Bizga Mijoz bo&apos;lanlarnina sotib olish imkoniyatiga ega bo&apos;lishadi</h1>
+      <h1 className="text-3xl font-bold mb-8 text-center">Bizga Mijoz bo&apos;lganlargina sotib olish imkoniyatiga ega bo&apos;lishadi</h1>
       <ContactForm />
     </main>
   );
